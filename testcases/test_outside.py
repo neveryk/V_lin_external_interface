efde9856938009b9cc65e47d3a5dd2d3805e4bd1
fff12888 <wu_yue121@163.com>
2023-06-09 09:29:19 +0800

    更新接口信息

--- testcases/test_outside.py
+++ testcases/test_outside.py
@@ -1,53 +1,58 @@
 from testcases.conftest import gen, outside_data
 import pytest
+import pytest_ordering
 import allure
+
 from common.operation_excel import excel
 from common.connect_database import s
 
-class TestOutside:
-    #apikey: str = None
 
 
 class TestOutside():
-    pass
-    V零工对外接口
-    每次必须调用apikey访问
+    #pass
+    #V零工对外接口
+    #每次必须调用apikey访问
     @allure.story('获取凭证接口')
     @allure.title('执行获取凭证接口用例')
     @allure.step('获取凭证')
     @pytest.mark.parametrize('userkey,secret,apitype,message,returnStatus',outside_data['test_getkey'])
-    @pytest.mark.run(order=3)
+    @pytest.mark.run(order=1)
     def test_getkey(self,userkey,secret,apitype,message,returnStatus):
         res=gen.get_key(userkey,secret,apitype)
         json_data=res.json()
+        print(json_data)
         global apikey
         apikey=json_data["data"]["apikey"]
         assert res.status_code==200
         assert json_data["message"]==message
         assert json_data["returnStatus"]==returnStatus
         return res
-    # #
-    # @allure.story('获取上传文件接口')
-    # @allure.title('上传文件接口')
-    # @allure.step('上传附件')
-    # def test_upload(self):
-    #     res=gen.upload_file(apikey)
-    #     json_data = res.json()
-    #     global file_path
-    #     file_path=json_data["data"][0]["path"]
-    #     assert res.status_code == 200
-    #     assert json_data["message"] == "success"
-    #     assert json_data["returnStatus"] == 1
-    #     return res
+    #
+    @allure.story('获取上传文件接口')
+    @allure.title('上传文件接口')
+    @allure.step('上传附件')
+    def test_upload(self):
+        res=gen.upload_file(apikey)
+        json_data = res.json()
+        global file_path
+        file_path=json_data["data"][0]["path"]
+        assert res.status_code == 200
+        assert json_data["message"] == "success"
+        assert json_data["returnStatus"] == 1
+        return res
     #
     # @allure.story('任务同步接口V1')
     # @allure.title('执行任务同步接口V1用例')
     # @allure.step('任务同步接口V1')
-    # @pytest.mark.parametrize("orderNo,orderName,customerCode,address,peopleName,iDCard,bankCode,amt,exAmt",outside_data["test_add_order_v1"])
-    # def test_add_order_v1(self,orderNo,orderName,customerCode,address,peopleName,iDCard,bankCode,amt,exAmt):
+    # @pytest.mark.parametrize("orderNo,orderName,customerCode,address,peopleName,iDCard,bankCode,amt,exAmt,message,returnStatus",outside_data["test_add_order_v1"])
+    # def test_add_order_v1(self,orderNo,orderName,customerCode,address,peopleName,iDCard,bankCode,amt,exAmt,message,returnStatus):
     #     res=gen.add_order_v1(apikey,orderNo,orderName,customerCode,address,peopleName,iDCard,bankCode,amt,exAmt)
+    #     json_data = res.json()
+    #     print(json_data)
+    #     assert json_data["message"] == message
+    #     assert json_data["returnStatus"] == returnStatus
     #     return res
-    #
+    # #
     # @allure.story('任务同步接口V2')
     # @allure.title('执行任务同步接口V2用例')
     # @allure.step('任务同步接口V2')
@@ -65,19 +70,19 @@ class TestOutside():
     #     data["apikey"] = apikey
     #     res = gen.many_add_order_v1(data)
     #     return res
-
-    # @allure.story('人员同步')
-    # @allure.title('执行人员同步用例')
-    # @allure.step('人员同步')
-    # @pytest.mark.parametrize("customerCode,peopleName,iDCard,phone,paymentAccountType,bankCode,bankName,createDate,signDate,signFile,protocolState", outside_data["test_add_people"])
-    # def test_add_people(self,customerCode,peopleName,iDCard,phone,paymentAccountType,bankCode,bankName,createDate,signDate,signFile,protocolState):
-    #     res = gen.add_people(apikey,customerCode,peopleName,iDCard,phone,paymentAccountType,bankCode,bankName,createDate,signDate,signFile,protocolState)
-    #     json_data = res.json()
-        # assert res.status_code == 200
-        # assert json_data["message"] == message
-        # assert json_data["returnStatus"] == returnStatus
-        # return res
-
+    #
+    @allure.story('人员同步')
+    @allure.title('执行人员同步用例')
+    @allure.step('人员同步')
+    @pytest.mark.parametrize("customerCode,peopleName,iDCard,phone,paymentAccountType,bankCode,bankName,createDate,signDate,signFile,protocolState", outside_data["test_add_people"])
+    def test_add_people(self,customerCode,peopleName,iDCard,phone,paymentAccountType,bankCode,bankName,createDate,signDate,signFile,protocolState):
+        res = gen.add_people(apikey,customerCode,peopleName,iDCard,phone,paymentAccountType,bankCode,bankName,createDate,signDate,signFile,protocolState)
+        json_data = res.json()
+        assert res.status_code == 200
+        assert json_data["message"] == message
+        assert json_data["returnStatus"] == returnStatus
+        return res
+    #
     # @allure.story('更改人员签约信息')
     # @allure.title('执行更改人员签约信息用例')
     # @allure.step('更改人员签约信息')
@@ -87,7 +92,7 @@ class TestOutside():
     #     json_data = res.json()
     #     assert json_data["message"] == "success"
     #     return res
-
+    #
     # @allure.story('银行卡修改')
     # @allure.title('执行银行卡修改用例')
     # @allure.step('银行卡修改')
@@ -99,7 +104,7 @@ class TestOutside():
     #     assert json_data["message"] == message
     #     assert json_data["returnStatus"] == returnStatus
     #     return res
-
+    #
     # @allure.story('获取账单状态')
     # @allure.title('执行获取账单状态用例')
     # @allure.step('获取账单状态')
@@ -107,7 +112,7 @@ class TestOutside():
     # def test_get_order_state(self,orderNo):
     #     res = gen.get_order_state(apikey,orderNo)
     #     return res
-
+    #
     # @allure.story('获取人员发放状态')
     # @allure.title('执行获取人员发放状态用例')
     # @allure.step('获取人员发放状态')
@@ -115,19 +120,19 @@ class TestOutside():
     # def test_get_people_state(self, generateCode):
     #     res = gen.get_people_state(apikey, generateCode)
     #     return res
-
-    @allure.step('获取企业可用余额')
-    @pytest.mark.parametrize("customerCode,returnStatus", outside_data["get_Client_freeamount"])
-    @pytest.mark.run(order=3)
-    def test_get_Client_freeamount(self, customerCode,returnStatus):
-        res = gen.get_Client_freeamount(apikey, customerCode)
-        json_data = res.json()
-        dfreeamount=s.getCfreeamount(customerCode)
-        assert json_data["data"]["accountBalance"]==dfreeamount[0][0]
-        assert json_data["message"]=="success"
-        assert json_data["returnStatus"]==returnStatus
-        return res
-
+    #
+    # @allure.step('获取企业可用余额')
+    # @pytest.mark.parametrize("customerCode,returnStatus", outside_data["get_Client_freeamount"])
+    # @pytest.mark.run(order=3)
+    # def test_get_Client_freeamount(self, customerCode,returnStatus):
+    #     res = gen.get_Client_freeamount(apikey, customerCode)
+    #     json_data = res.json()
+    #     dfreeamount=s.getCfreeamount(customerCode)
+    #     assert json_data["data"]["accountBalance"]==dfreeamount[0][0]
+    #     assert json_data["message"]=="success"
+    #     assert json_data["returnStatus"]==returnStatus
+    #     return res
+    #
     # @allure.step('人员发单状态查询')
     # @pytest.mark.parametrize("iDCard,peopleName,phone,bankCode,message,status",outside_data["get_people_orderstatus"])
     # @pytest.mark.run(order=2)
@@ -142,13 +147,13 @@ class TestOutside():
     #     assert json_data["data"]["status"]==status
     #     return res
 
-    # @allure.step('对账接口')
-    # @pytest.mark.parametrize("customerCode,startDate,endDate,message,returnStatus", outside_data["get_Statement"])
-    # @pytest.mark.run(order=4)
-    # def test_get_Statement(self, customerCode, startDate, endDate,message,returnStatus):
-    #     res = gen.get_Statement(apikey, customerCode, startDate, endDate)
-    #     json_data = res.json()
-    #     print(json_data)
-    #     assert json_data["message"]==message
-    #     assert json_data["returnStatus"]==returnStatus
-    #     return res
+    @allure.step('对账接口')
+    @pytest.mark.parametrize("customerCode,startDate,endDate,message,returnStatus", outside_data["get_Statement"])
+    @pytest.mark.run(order=2)
+    def test_get_Statement(self, customerCode, startDate, endDate,message,returnStatus):
+        res = gen.get_Statement(apikey, customerCode, startDate, endDate)
+        json_data = res.json()
+        print(json_data)
+        assert json_data["message"]==message
+        assert json_data["returnStatus"]==returnStatus
+        return res
